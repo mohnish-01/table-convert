809f2b1c2aa50391dd3e6c2959ecd5a340c48c15
Mohnish <mohnishgehlot.mg@gmail.com>
2025-07-09 21:45:15 +0530

    modified script to read input and output file from command line arguments

--- table-convert.py
+++ table-convert.py
@@ -1,3 +1,24 @@
 import sys
 import re
 
+def convert(infile, outfile):
+    try:
+        with open(infile, 'r') as inputobj:
+            print(inputobj.readline())
+    except FileNotFoundError as e:
+        print(f"Exception Occured: {e}")
+        sys.exit(2)
+    except IOError as e:
+        print(f"Exception Occured: {e}")
+        sys.exit(2)
+
+
+def main():
+    if len(sys.argv) < 3 or len(sys.argv) > 3:
+        print(f"Usage: {sys.argv[0]} inputfile outputfile")
+        sys.exit(2)
+    else:
+        convert(sys.argv[1], sys.argv[2])
+
+if __name__ == "__main__":
+    main()
